2734a454e26374c1b08afc53d62ceb071c022f13
AliciaT <aliciaju@126.com>
2023-12-09 02:40:28 +0100

    with axises check

--- separate and merge/script/interact.js
+++ separate and merge/script/interact.js
@@ -57,6 +57,43 @@ function getfunctionflag(){
     return funcFlag;
 };
 
+function InhaltVorarbeiten(txt_inhalt){
+    if(txt_inhalt != ""){
+        let first_enter = txt_inhalt.indexOf("\n");
+        let first_line = txt_inhalt.substring(0, first_enter);
+        let coorindat = /^-?([1-9]d.d|0.d[1-9]d|0?.0+|0),-?([1-9]d.d|0.d[1-9]d|0?.0+|0)$/;
+        if(!first_line.match(coorindat)){
+            console.log("axis include");
+            txt_inhalt = txt_inhalt.substring(first_enter);
+        }
+        return txt_inhalt;
+    }   
+}
+
+/* Not complited */
+
+function MapViewSwitcher(){
+    let mapHTMLCtn = document.getElementById(mapContainer);
+    let mapWindows = document.getElementById("map_iframe");
+    let switchbutton = document.getElementById("switchbtn");
+    if(switchbutton.innerHTML == "Full Screen"){
+        alert("change to full");
+        mapWindows.style.transform = "scale(1)";
+        //mapWindows.style.left = "0px";
+        //mapWindows.style.top = "0px";
+        mapHTMLCtn.style.gridArea= "1/1/2/2";
+        switchbutton.innerHTML = 'Exit Full Screen';
+        return 0;
+    }else{
+        mapWindows.style.transform = "scale(0.6)";
+        mapWindows.style.left = "-240px";
+        mapWindows.style.top = "-145px";
+        mapWindows.style.zIndex = 4;
+        mapWindows.value = "preview";
+        return 0;
+    }
+}
+
 
 /*+++++++++++++++++++ functions from other grupps +++++++++++++++++++*/ 
 function initializeMap() {
@@ -74,7 +111,7 @@ function dealwithrun(){
     punktdata = getinputdata();
     let functionFlag = getfunctionflag();
     if(punktdata != ""){
-        alert("Start Map initialising");
+        InhaltVorarbeiten(punktdata);
         hideprepera();
         showresult();
         initializeMap();
